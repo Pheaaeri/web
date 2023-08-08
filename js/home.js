b46c7941832dd0e36e3311165031a89aafced591
Pheaaeri <phea.aeri@gmail.com>
2023-08-08 16:17:47 +0700

    up

--- js/home.js
+++ js/home.js
@@ -1,7 +1,32 @@
+
+var userlog = localStorage.getItem("username");
 document.getElementById("logout").onclick = function(){
     localStorage.clear()
     window.location = "login.html"
 
 }
+//loadDataDirectorJSON();
+function loadDataDirectorJSON() {
+	var dataJSON = "https://script.google.com/macros/s/AKfycbyeUDT7e_0C4FMvoqtuu41ypXaOqPfoZqDGVyugEniQetUqHmD8hXLPFriLUlSOZt_TsQ/exec";
+	// get ค่าจาก url
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", dataJSON, false); // false for synchronous request
+	xmlHttp.send(null);
+	obj = xmlHttp.responseText;
 
-var userlog = localStorage.getItem("username");
+	// แปลง string เป็น JSON
+	objPeople = JSON.parse(xmlHttp.responseText);
+    
+    //
+    let uidname = objPeople.find();
+
+    //
+    let dnameid = uidname.dname
+    let uidname1 = document.getElementById("iddname");
+    uidname1.innerHTML = dnameid + " วัน";
+    //document.getElementById("lsday1").value = lsday
+
+    
+}
+
+console.log(objPeople);
