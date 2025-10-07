78486b17bb5d2d8f5d344ef5620601a7e2f0f35b
Pheaaeri <phea.aeri@gmail.com>
2025-10-07 12:10:48 +0700

    show name

--- js/home.js
+++ js/home.js
@@ -1,28 +1,28 @@
 
 var userlog = localStorage.getItem("username");
-document.getElementById("logout").onclick = function(){
+document.getElementById("logout").onclick = function () {
     localStorage.clear()
     window.location = "login.html"
 
 }
 loadDataDirectorJSON();
 function loadDataDirectorJSON() {
-	var dataJSON = "https://script.google.com/macros/s/AKfycbyeUDT7e_0C4FMvoqtuu41ypXaOqPfoZqDGVyugEniQetUqHmD8hXLPFriLUlSOZt_TsQ/exec";
-	// get ค่าจาก url
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", dataJSON, false); // false for synchronous request
-	xmlHttp.send(null);
-	obj = xmlHttp.responseText;
+    var dataJSON = "https://script.google.com/macros/s/AKfycbyeUDT7e_0C4FMvoqtuu41ypXaOqPfoZqDGVyugEniQetUqHmD8hXLPFriLUlSOZt_TsQ/exec";
+    // get ค่าจาก url
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", dataJSON, false); // false for synchronous request
+    xmlHttp.send(null);
+    obj = xmlHttp.responseText;
 
-	// แปลง string เป็น JSON
-	objPeople = JSON.parse(xmlHttp.responseText);
+    // แปลง string เป็น JSON
+    objPeople = JSON.parse(xmlHttp.responseText);
     //
     let dnameid = objPeople[0].dname
     document.getElementById("dname").innerHTML = "ผู้อำนวยการ(ผู้อนุมัติ) : " + dnameid
     //
     let ddeatid = objPeople[0].ddeta
     document.getElementById("ddeta").innerHTML = ddeatid
-    
+
     //
     //let uidname = objPeople.find();
 
@@ -31,6 +31,18 @@ function loadDataDirectorJSON() {
     //let uidname1 = document.getElementById("iddname");
     //uidname1.innerHTML = dnameid + " วัน";
     //document.getElementById("lsday1").value = lsday
-    
+
     console.log(ddeatid);
 }
+
+//*gfhfghfghf
+function Loading() {
+    setTimeout(function () {
+        Swal.fire({
+            title: objPeople[0].dname, //ค่า ผู้อำนวยการ(ผู้อนุมัติ)
+            text: objPeople[0].ddeta, //ค่า ตำแห่นงผู้อำนวยการ(ผู้อนุมัติ)
+            confirmButtonText: 'รับทราบ',
+            showConfirmButton: true //True เปิด - False ปิด แสดงปุ่มกด
+        })
+    });
+};
